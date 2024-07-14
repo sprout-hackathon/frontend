acbb9299a1083287f355fb3aa3e7f6829204a219
sohyoeun <she020108@gmail.com>
2024-07-14 13:32:56 +0900

    prettier & eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,42 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.css'],
+      },
+    },
+  },
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    'react-refresh',
+    'prettier',
+  ],
   rules: {
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': 'error',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'warn',
+    eqeqeq: ['error', 'always'],
   },
 };

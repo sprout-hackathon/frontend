7918d14eea600d787327b756663d2ac5149401b7
she1018 <she020108@gmail.com>
2024-07-15 15:33:10 +0900

    fix: BottomTab이 특정 페이지에서만 표시되도록 수정

--- src/components/common/BottomTab/BottomTab.jsx
+++ src/components/common/BottomTab/BottomTab.jsx
@@ -9,37 +9,49 @@ import communityBlueIcon from '../../../assets/icons/community-blue.svg';
 import mypageGrayIcon from '../../../assets/icons/mypage-gray.svg';
 import mypageBlueIcon from '../../../assets/icons/mypage-blue.svg';
 import ChatbotButton from './ChatbotButton';
+import { useLocation } from 'react-router-dom';
 
 const BottomTab = () => {
-  return (
-    <div className='absolute flex h-20 w-full flex-row items-center justify-stretch rounded-t-xl border bg-white'>
-      <BottomTabButton
-        text='홈'
-        path='/'
-        blueIcon={homeBlueIcon}
-        grayIcon={homeGrayIcon}
-      />
-      <BottomTabButton
-        text='챗봇 내역'
-        path='/history'
-        blueIcon={historyBlueIcon}
-        grayIcon={historyGrayIcon}
-      />
-      <ChatbotButton />
-      <BottomTabButton
-        text='커뮤니티'
-        path='/community'
-        blueIcon={communityBlueIcon}
-        grayIcon={communityGrayIcon}
-      />
-      <BottomTabButton
-        text='마이페이지'
-        path='/mypage'
-        blueIcon={mypageBlueIcon}
-        grayIcon={mypageGrayIcon}
-      />
-    </div>
-  );
+  const location = useLocation();
+
+  if (
+    (location.pathname === '/') |
+    (location.pathname === '/history') |
+    (location.pathname === '/community') |
+    (location.pathname === '/mypage')
+  ) {
+    return (
+      <div className='absolute flex h-20 w-full flex-row items-center justify-stretch rounded-t-xl border bg-white'>
+        <BottomTabButton
+          text='홈'
+          path='/'
+          blueIcon={homeBlueIcon}
+          grayIcon={homeGrayIcon}
+        />
+        <BottomTabButton
+          text='챗봇 내역'
+          path='/history'
+          blueIcon={historyBlueIcon}
+          grayIcon={historyGrayIcon}
+        />
+        <ChatbotButton />
+        <BottomTabButton
+          text='커뮤니티'
+          path='/community'
+          blueIcon={communityBlueIcon}
+          grayIcon={communityGrayIcon}
+        />
+        <BottomTabButton
+          text='마이페이지'
+          path='/mypage'
+          blueIcon={mypageBlueIcon}
+          grayIcon={mypageGrayIcon}
+        />
+      </div>
+    );
+  }
+
+  return <></>;
 };
 
 export default BottomTab;

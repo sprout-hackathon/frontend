e767e6dfe54a91ca0a2108fd6e1e05039da9d99a
she1018 <she020108@gmail.com>
2024-07-15 10:28:03 +0900

    chore: tailwindcss 커스텀 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: { DEFAULT: '#4457FC', light: '#C7CDFF' },
+      yellow: {
+        DEFAULT: '#FFDA19',
+      },
+      red: '#F22929',
+      black: '#000000',
+      white: '#FFFFFF',
+      gray: {
+        50: '#fafafa',
+        100: '#f4f4f5',
+        200: '#e4e4e7',
+        300: '#d4d4d8',
+        400: '#a1a1aa',
+        500: '#71717a',
+        600: '#52525b',
+        700: '#3f3f46',
+        800: '#27272a',
+        900: '#18181b',
+        950: '#09090b',
+      },
+    },
   },
   plugins: [],
 };

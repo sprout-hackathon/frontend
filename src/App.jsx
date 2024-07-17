e877b2f9fddb746847c52a17dbc90401ef60ae98
dongckim <dck.alx@gmail.com>
2024-07-17 15:06:40 +0900

    feat: 로그인페이지 UI구현, 데이터 fetching 미완

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import BottomTab from './components/common/BottomTab/BottomTab';
 import HomeDetail from './pages/HomeDetail';
 import Chatbot from './pages/Chatbot';
+import Login from './pages/Login'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>}/>
         <Route path='/detail/:id' element={<HomeDetail />} />
         <Route path='/chatbot' element={<Chatbot />} />
       </Routes>

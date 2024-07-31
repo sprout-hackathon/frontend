99b708ddb5435b0730d1285908aaf6686ef3c0f9
she1018 <she020108@gmail.com>
2024-07-31 22:35:19 +0900

    feat: 지원내역 API 연결 중

--- src/pages/ApplyList.jsx
+++ src/pages/ApplyList.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import Modal from '../components/common/Modal/Modal';
 import ModalText from '../components/common/Modal/ModalText';
 import ModalButton from '../components/common/Modal/ModalButton';
+import { useQuery } from '@tanstack/react-query';
+import { getApplicationList } from '../api/applications';
 
 const dataList = [0, 1, 2, 3, 4];
 
@@ -13,6 +15,33 @@ const ApplyList = () => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   let showModal = 0 <= selectedIndex && selectedIndex < dataList.length;
   const navigate = useNavigate();
+
+  const ListContainer = () => {
+    const { data, isPending, isError } = useQuery({
+      queryKey: ['applicationList'],
+      queryFn: getApplicationList,
+    });
+
+    if (isPending) return <ul className='flex flex-col gap-4'></ul>;
+    if (isError) return <ul className='flex flex-col gap-4'></ul>;
+
+    return (
+      <ul className='flex flex-col gap-4'>
+        {data.map((item, index) =>
+          index === selectedIndex ? (
+            <ApplyCard key={index} data={item} selected />
+          ) : (
+            <ApplyCard
+              key={index}
+              data={item}
+              disabled={!isEditing}
+              onSelect={() => setSelectedIndex(index)}
+            />
+          )
+        )}
+      </ul>
+    );
+  };
 
   const handleEdit = () => {
     setEditing((editing) => !editing);
@@ -23,7 +52,7 @@ const ApplyList = () => {
   };
 
   return (
-    <div className='flex h-dvh flex-col overflow-y-auto pb-5'>
+    <div className='flex h-dvh flex-col'>
       <div className='flex w-full flex-row items-center px-5 py-4'>
         <button onClick={() => navigate(-1)} className='mr-8'>
           <img src={leftChevronIcon} alt='back icon' />
@@ -39,28 +68,17 @@ const ApplyList = () => {
           </button>
         )}
       </div>
-      <ul className='flex flex-col gap-4'>
-        {dataList.map((data, index) =>
-          index === selectedIndex ? (
-            <ApplyCard key={index} data={data} selected />
-          ) : (
-            <ApplyCard
-              key={index}
-              data={data}
-              disabled={!isEditing}
-              onSelect={() => setSelectedIndex(index)}
-            />
-          )
+      <div className='overflow-y-auto pb-5'>
+        <ListContainer />
+        {showModal && (
+          <Modal onClickClose={handleModalClose}>
+            <ModalText text='지원 현황을 수정해주세요' />
+            <ModalButton text='지원중' />
+            <ModalButton text='합격' />
+            <ModalButton text='탈락' />
+          </Modal>
         )}
-      </ul>
-      {showModal && (
-        <Modal onClickClose={handleModalClose}>
-          <ModalText text='지원 현황을 수정해주세요' />
-          <ModalButton text='지원중' />
-          <ModalButton text='합격' />
-          <ModalButton text='탈락' />
-        </Modal>
-      )}
+      </div>
     </div>
   );
 };
